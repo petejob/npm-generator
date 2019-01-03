ae4fb4bc6d6c3b074731e4cc64f54fe180788ef0
ptjob1 <ptjob6@gmail.com>
2019-01-03 17:22:20 +0000

    Update package.json to include project name

--- generator.js
+++ generator.js
@@ -1,55 +1,70 @@
 #!/usr/bin/env node
-const fs = require('fs');
+const fs = require(`fs`);
+const inquirer = require(`inquirer`);
 
-fs.mkdir(`${process.cwd()}/projects`, err => {
-  if (err) throw err;
-  console.log('The projects directory has been created');
-  fs.writeFile('projects/index.js', '', err => {
+const QUESTIONS = [
+  {
+    name: `project-name`,
+    type: "input",
+    message: `Project name: `,
+    validate: function(input) {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else
+        return "Project name may only include letters, numbers, underscores and hashes.";
+    }
+  }
+];
+
+inquirer.prompt(QUESTIONS).then(answers => {
+  const projectName = answers[`project-name`];
+
+  createProject(projectName);
+});
+
+function createProject(projectName) {
+  fs.mkdir(`${process.cwd()}/${projectName}`, err => {
     if (err) throw err;
-    console.log('File index.js has been created successfully!');
-  });
-  fs.mkdir(`${process.cwd()}/projects/spec`, err => {
-    if (err) throw err;
-    console.log('The spec directory has been created');
+    fs.writeFile(`${projectName}/index.js`, "", err => {
+      if (err) throw err;
+    });
+    fs.mkdir(`${process.cwd()}/${projectName}/spec`, err => {
+      if (err) throw err;
+      fs.writeFile(
+        `${process.cwd()}/${projectName}/spec/index.spec.js`,
+        "const chai = require(`chai`);",
+        err => {
+          if (err) throw err;
+        }
+      );
+    });
+    fs.writeFile(`${process.cwd()}/${projectName}/.gitignore`, "", err => {
+      if (err) throw err;
+    });
     fs.writeFile(
-      `${process.cwd()}/projects/spec/index.spec.js`,
-      'const chai = require("chai");',
+      `${process.cwd()}/${projectName}/package.json`,
+      `{
+      "name": `${projectName}`,
+      "version": "1.0.0",
+      "description": "",
+      "main": "generator.js",
+      "scripts": {
+        "test": "mocha ./spec/"
+      },
+      "devDependencies": {
+        "chai": "^3.5.0",
+        "eslint": "^3.15.0"
+      },
+      "keywords": [],
+      "author": "",
+      "license": "ISC"
+    }
+    `,
       err => {
         if (err) throw err;
-        console.log('The spec file has been successfully created!');
       }
     );
-  });
-  fs.writeFile(`${process.cwd()}/projects/.gitignore`, '', err => {
-    if (err) throw err;
-    console.log('File .gitignore has been created successfully!');
-  });
-  fs.writeFile(
-    `${process.cwd()}/projects/package.json`,
-    `{
-    "name": "ProjectGenarator",
-    "version": "1.0.0",
-    "description": "",
-    "main": "generator.js",
-    "scripts": {
-      "test": "mocha ./spec/"
-    },
-    "devDependencies": {
-      "chai": "^3.5.0",
-      "eslint": "^3.15.0"
-    },
-    "keywords": [],
-    "author": "",
-    "license": "ISC"
-  }
-  `,
-    err => {
+    fs.writeFile(`${process.cwd()}/${projectName}/READ.md`, "", err => {
       if (err) throw err;
-      console.log('File package.json has been created successfully!');
-    }
-  );
-  fs.writeFile(`${process.cwd()}/projects/READ.md`, '', err => {
-    if (err) throw err;
-    console.log('File READ.md has been created successfully!');
+    });
   });
-});
+}
